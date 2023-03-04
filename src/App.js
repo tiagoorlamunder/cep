d33905df67fa77d759db724aa06ab28f9e715400
tiagoorlamunder <tiago.orlamunder@gmail.com>
2023-03-04 02:14:56 -0300

    Configured styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from "react-icons/fi";
+import './styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Localizador</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="CEP..."></input>
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+      <main className="main">
+        <h2>CEP: 89211230</h2>
+        <span>Rua Paulo Setubal</span>
+        <span>Complemento: Casa</span>
+        <span>Bairro Floresta</span>
+        <span>Joinville - SC</span>
+      </main>
     </div>
   );
 }
